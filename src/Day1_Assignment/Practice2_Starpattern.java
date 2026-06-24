ab1773f4add68f3b39a853b867983f60f7e60ef5
Komal <komal landge>
2026-06-24 17:04:37 +0530

    Start pattern and typecasting

---
+++ src/Day1_Assignment/Practice2_Starpattern.java
@@ -0,0 +1,15 @@
+package Day1_Assignment;
+
+public class Practice2_Starpattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("*");
+		System.out.println("**");
+		System.out.println("***");
+		System.out.println("****");
+		System.out.println("*****");
+	}
+
+}
---
+++ src/Day1_Assignment/Practice2_typecasting.java
@@ -0,0 +1,16 @@
+package Day1_Assignment;
+
+public class Practice2_typecasting {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		double height=10.72;
+		int heightInt=(int)height;
+		System.out.println("Double value "+height);
+		System.out.println("int value " +heightInt);
+		
+	}
+
+}
