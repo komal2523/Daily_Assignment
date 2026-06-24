5e904b6fb6ec9730d1c950cfa8adcbd291621a47
Komal <komal landge>
2026-06-24 17:01:43 +0530

    ASCI value program

---
+++ src/Day1_Assignment/ASCII_value.java
@@ -0,0 +1,30 @@
+package Day1_Assignment;
+
+public class ASCII_value {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char alpha='A';
+		int value=alpha;
+		System.out.println("ASCII VALUE of A =" +value);
+		
+		
+		char alpha1='a';
+		int value1=alpha1;
+		System.out.println("ASCII VALUE of a =" +value1);
+		
+		char alpha2='0';
+		int value2=alpha2;
+		System.out.println("ASCII VALUE of 0 =" +value2);
+		
+		char alpha3='@';
+		int value3=alpha3;
+		System.out.println("ASCII VALUE of @ =" +value3);
+		
+	
+
+
+
+	}
+
+}
