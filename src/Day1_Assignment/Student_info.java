772230257f1b0535904bdeeae4326f695530f266
Komal <komal landge>
2026-06-24 16:23:27 +0530

    The first commit from local

---
+++ src/Day1_Assignment/Student_info.java
@@ -0,0 +1,30 @@
+package Day1_Assignment;
+
+public class Student_info {
+
+	int Age =18;
+	int ID =101;
+	boolean results =true;
+	char Grade ='A';
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stu
+		
+		{
+			System.out.println("Student ID = 101");
+			
+			
+			System.out.println("Grade = A");
+			
+			
+			System.out.println("Passed = true");
+
+			
+			System.out.println("Age = 18");
+			
+				
+		}
+
+	}
+}
+	
